51f3b8bd247e4d3c510dd6fd16c6514923c3566e
Asher <asher.rosenblatt@me.com>
2018-04-03 14:55:57 -0500

    Initial Commit

---
+++ WagCodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WagCodingChallenge
+//
+//  Created by Asher Rosenblatt on 4/3/18.
+//  Copyright © 2018 AVRTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WagCodingChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WagCodingChallenge
+//
+//  Created by Asher Rosenblatt on 4/3/18.
+//  Copyright © 2018 AVRTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
